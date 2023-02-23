774285fa8191929b4e7d5925915388b511aecbee
mariacasanova25 <mariajoao.albino25@gmail.com>
2023-02-23 18:22:22 +0100

    2 errors fixed

--- ChatImpl.java
+++ ChatImpl.java
@@ -6,9 +6,9 @@ import java.util.*;
 import java.nio.*;
 import java.io.*;
 
-public  class ChatImpl implements ChatService {
+public class ChatImpl implements ChatService {
 
-	//HashMap<Integer, InfoClient> registrationTable;
+    // HashMap<Integer, InfoClient> registrationTable;
     File users;
     File history;
     ArrayList<String> historyArray;
@@ -17,130 +17,129 @@ public  class ChatImpl implements ChatService {
     BufferedReader readerHistory;
     BufferedReader readerUsers;
 
-
- 
-	public ChatImpl() {
-		//registrationTable= new HashMap<Integer, InfoClient>();
+    public ChatImpl() {
+        // registrationTable= new HashMap<Integer, InfoClient>();
         historyArray = new ArrayList<String>();
-        users= new File("users.txt");
-        history= new File("history.txt");
+        users = new File("users.txt");
+        history = new File("history.txt");
 
-        fillHistory();      
-	}
+        fillHistory();
+    }
 
-    private void fillHistory(){
+    private void fillHistory() {
 
-      try {
-            readerHistory = new BufferedReader(new FileReader(history)) ;
+        try {
+            readerHistory = new BufferedReader(new FileReader(history));
             String line = readerHistory.readLine();
-            if(line!=null) historyArray.add(line);
+            if (line != null)
+                historyArray.add(line);
             while (line != null) {
                 line = readerHistory.readLine();
                 historyArray.add(line);
             }
             readerHistory.close();
-      }catch(IOException e){
-       e.printStackTrace(); 
-      }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-   
-	public void join(InfoClient client)  throws RemoteException, Exception{
 
-        if(isUserJoined(client)) throw new Exception("user already exists"); //user already exists
+    public void join(InfoClient client) throws RemoteException, Exception {
 
-          try{
-            writerUsers= new FileWriter(users, true);
-            //TODO: fix \n on first join
-            writerUsers.append("\n"+Integer.toString(client.getId()));
+        if (isUserJoined(client))
+            throw new Exception("user already exists"); // user already exists
+
+        try {
+            writerUsers = new FileWriter(users, true);
+            // TODO: fix \n on first join
+            writerUsers.append("\n" + Integer.toString(client.getId()));
             writerUsers.close();
-           
-        }catch (IOException e){
+
+        } catch (IOException e) {
             System.out.println("Unable to open history");
             System.exit(-1);
         }
-       
+
     }
-   
-    public void leave(InfoClient client) throws RemoteException, Exception{
+
+    public void leave(InfoClient client) throws RemoteException, Exception {
         System.out.println("entrou em leave");
-        if(!isUserJoined(client)) throw new Exception("This user doesn't exist"); //user doesn't exist
-          
-        try{
-            removeUser(Integer.toString(client.getId())); 
+        if (!isUserJoined(client))
+            throw new Exception("This user doesn't exist"); // user doesn't exist
+
+        try {
+            removeUser(Integer.toString(client.getId()));
             System.out.println("user removed");
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Unable to open history");
             System.exit(-1);
         }
     }
 
-    public void send(String message, InfoClient sender) throws RemoteException, Exception{
-        if(!isUserJoined(sender)) throw new Exception("You are not in the chatgroup");
+    public void send(String message, InfoClient sender) throws RemoteException, Exception {
+        if (!isUserJoined(sender))
+            throw new Exception("You are not in the chatgroup");
 
-        try{
-            writer= new FileWriter(history, true);
-            String newMessage="Sender: "+Integer.toString(sender.getId())+" plaintext: " +message;
-            writer.append("\n"+newMessage);
+        try {
+            writer = new FileWriter(history, true);
+            String newMessage = "Sender: " + Integer.toString(sender.getId()) + " plaintext: " + message;
+            writer.append("\n" + newMessage);
             historyArray.add(newMessage);
             writer.close();
 
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Unable to open history");
             System.exit(-1);
         }
     }
 
-    //TODO:fix array null lines 
-    public ArrayList<String> getHistory(InfoClient client) throws RemoteException, Exception{
-        if(!isUserJoined(client)) throw new Exception("error: you're not in the group"); 
+    // TODO:fix array null lines
+    public ArrayList<String> getHistory(InfoClient client) throws RemoteException, Exception {
+        if (!isUserJoined(client))
+            throw new Exception("error: you're not in the group");
         return historyArray;
     }
 
-    public boolean isUserJoined(InfoClient client){
-       String line;
+    public boolean isUserJoined(InfoClient client) {
+        String line;
         try {
-            readerUsers= new BufferedReader(new FileReader(users)) ;
-          
-            while ((line=readerUsers.readLine()) != null) {
-                if(line.equals(Integer.toString(client.getId()))) {
-                   
-                    System.out.println("encontrou user"); 
+            readerUsers = new BufferedReader(new FileReader(users));
+
+            while ((line = readerUsers.readLine()) != null) {
+                if (line.equals(Integer.toString(client.getId()))) {
+
+                    System.out.println("encontrou user");
                     return true;
                 }
-            } 
+            }
             readerUsers.close();
             return false;
 
-        }catch(IOException e){
-             System.out.println("isuserjoin"+ e.getMessage());
-            e.printStackTrace(); 
+        } catch (IOException e) {
+            System.out.println("isuserjoin" + e.getMessage());
+            e.printStackTrace();
         }
-      return false;
+        return false;
     }
 
-
-    public void removeUser(String lineContent) throws IOException
-    {
+    public void removeUser(String lineContent) throws IOException {
         File updatedUsersFile = new File("updatedUsers.txt");
 
         readerUsers = new BufferedReader(new FileReader(users));
-        BufferedWriter w = new BufferedWriter (new FileWriter(updatedUsersFile));
+        BufferedWriter w = new BufferedWriter(new FileWriter(updatedUsersFile));
 
         String currentLine;
 
-        while((currentLine = readerUsers.readLine()) != null) {
+        while ((currentLine = readerUsers.readLine()) != null) {
             // trim newline when comparing with lineToRemove
             String trimmedLine = currentLine.trim();
-            if(!trimmedLine.equals(lineContent)) {
+            if (!trimmedLine.equals(lineContent)) {
                 w.write(currentLine + System.getProperty("line.separator"));
             }
         }
 
-        w.close(); 
-        readerUsers.close(); 
+        w.close();
+        readerUsers.close();
         updatedUsersFile.renameTo(users);
     }
 
-    
 }
-
--- Client.java
+++ Client.java
@@ -1,77 +1,77 @@
 import java.rmi.*;
 import java.rmi.registry.*;
-import java.rmi.server.*; 
+import java.rmi.server.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
-  public static void main(String [] args) {
-	
-	try {
-	  if (args.length < 2) {
-	   System.out.println("Usage: java HelloClient <rmiregistry host>");
-	   return;
-	   }
+    public static void main(String[] args) {
 
-		String host = args[0];
-		
-		InfoClientImpl client= new InfoClientImpl(Integer.parseInt(args[1]));
-		InfoClient infoClient= (InfoClient) UnicastRemoteObject.exportObject(client, 0);
-		Registry registry = LocateRegistry.getRegistry(host); 
+        try {
+            if (args.length < 2) {
+                System.out.println("Usage: java HelloClient <rmiregistry host>");
+                return;
+            }
 
-		ChatService chatService = (ChatService) registry.lookup("ChatService");
+            String host = args[0];
 
-		Scanner scanner = new Scanner(System.in);
-		String line;
-		String[] command;
-		int res;
-		
-		while(true){
+            InfoClientImpl client = new InfoClientImpl(Integer.parseInt(args[1]));
+            InfoClient infoClient = (InfoClient) UnicastRemoteObject.exportObject(client, 0);
+            Registry registry = LocateRegistry.getRegistry(host);
 
-			line = scanner.nextLine();
-			command = line.split(" ");
-			
-			try{
-				switch(command[0]){
-					
-					case "send":
-							chatService.send(command[1],infoClient);
-							System.out.println("success: message sent");
-						
-						break;
+            ChatService chatService = (ChatService) registry.lookup("ChatService");
 
-					case "join":
-							chatService.join(infoClient);
-							System.out.println("success: you joined the group");
-						break;
+            Scanner scanner = new Scanner(System.in);
+            String line;
+            String[] command;
+            int res;
 
-					case "leave":
-							chatService.leave(infoClient);
-							System.out.println("success: you left the group");
-						break;
+            while (true) {
 
-					case "history":
-						
-							ArrayList<String> history= chatService.getHistory(infoClient);
+                line = scanner.nextLine();
+                command = line.split(" ");
 
-							if(history.size()!=0 ){
-								for(int i=0; i<history.size(); i++){
-									System.out.println(history.get(i));
-								}
-							}else{
-								System.out.println("no messages yet");
-							}
-						break;
-		
-				}
-			}catch(Exception e){
-				System.out.println("SWITCH ERROR");
-				System.out.println(e.getMessage());
-			}
-		}
-	
-	} catch (Exception e)  {
-		System.err.println("Error on client: " + e);
-	}
-  }
+                try {
+                    switch (command[0]) {
+
+                        case "send":
+                            chatService.send(command[1], infoClient);
+                            System.out.println("success: message sent");
+
+                            break;
+
+                        case "join":
+                            chatService.join(infoClient);
+                            System.out.println("success: you joined the group");
+                            break;
+
+                        case "leave":
+                            chatService.leave(infoClient);
+                            System.out.println("success: you left the group");
+                            break;
+
+                        case "history":
+
+                            ArrayList<String> history = chatService.getHistory(infoClient);
+
+                            if (history.size() != 0) {
+                                for (int i = 0; i < history.size(); i++) {
+                                    System.out.println(history.get(i));
+                                }
+                            } else {
+                                System.out.println("no messages yet");
+                            }
+                            break;
+
+                    }
+                } catch (Exception e) {
+                    System.out.println("SWITCH ERROR");
+                    System.out.println(e.getMessage());
+                }
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error on client: " + e);
+        }
+    }
 }
